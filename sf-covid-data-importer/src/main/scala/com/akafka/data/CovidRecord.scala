fb745d142b8c30268cd8e1995af9c9305ad32d14
Adam Kafka <adkafka@gmail.com>
2021-01-03 15:47:22 -0800

    Refactor SF section before adding vaccination data

--- sf-covid-data-importer/src/main/scala/com/akafka/data/CovidRecord.scala
+++ sf-covid-data-importer/src/main/scala/com/akafka/data/CovidRecord.scala
@@ -1,14 +1,29 @@
 package com.akafka.data
 
+import akka.http.scaladsl.model.DateTime
+import com.akafka.data.CovidRecord.EpochMs
+import io.circe.{Decoder, DecodingFailure, HCursor}
+import io.circe.Decoder.Result
 import io.circe.generic.extras.{Configuration, ConfiguredJsonCodec}
 
 object CovidRecord {
-  implicit val config: Configuration = Configuration.default.withSnakeCaseMemberNames
+  type EpochMs = Long
 
+  def dateToEpochMs(date: String, cursor: HCursor): Result[EpochMs] = {
+    DateTime.fromIsoDateTimeString(date + "Z")
+      .map(_.clicks)
+      .toRight(DecodingFailure(s"Bad date format on $date", cursor.history))
+  }
+
+  implicit val epochMsDecoder: Decoder[EpochMs] = Decoder.instance { cursor =>
+    cursor.as[String].flatMap(dateToEpochMs(_, cursor))
+  }
+
+  implicit val config: Configuration = Configuration.default.withSnakeCaseMemberNames
 }
 
 @ConfiguredJsonCodec  final case class CovidRecord(
-  specimenCollectionDate: String,
+  specimenCollectionDate: EpochMs,
   caseDisposition: String,
   transmissionCategory: String,
   caseCount: String,
--- sf-covid-data-importer/src/main/scala/com/akafka/transformation/SfCovid.scala
+++ sf-covid-data-importer/src/main/scala/com/akafka/transformation/SfCovid.scala
@@ -1,6 +1,5 @@
 package com.akafka.transformation
 
-import akka.http.scaladsl.model.DateTime
 import com.akafka.data.CovidRecord
 
 import scala.util.Try
@@ -8,10 +7,6 @@ import scala.util.Try
 object SfCovid {
   val CasesMeasurement = "cases"
   val DeathsMeasurement = "deaths"
-
-  def dateToEpochMs(date: String): Either[String, Long] = {
-    DateTime.fromIsoDateTimeString(date + "Z").map(_.clicks).toRight(s"Bad date format on $date")
-  }
 
   def toInfluxProtocol(records: List[CovidRecord]): Either[String, List[String]] = {
     import cats.implicits._
@@ -25,15 +20,13 @@ object SfCovid {
       case "Death" => Right(DeathsMeasurement)
     }
 
-    val date = dateToEpochMs(record.specimenCollectionDate)
     val caseCount = Try(record.caseCount.toInt).toEither.left.map(_.toString)
     val lastUpdated = "\"" + record.lastUpdatedAt + "\""
     val transmission = record.transmissionCategory.replaceAll("\\s","")
 
     for {
-      date <- date
       m <- measurement
       c <- caseCount
-    } yield s"$m,transmission_category=$transmission case_count=$c,last_updated=$lastUpdated $date"
+    } yield s"$m,transmission_category=$transmission case_count=$c,last_updated=$lastUpdated ${record.specimenCollectionDate}"
   }
 }
